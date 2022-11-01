b5ef7287dcf457422ab51b482df6add33c2f0309
krishkpatil <patilkkrish@gmail.com>
2022-11-01 22:56:38 +0530

    commit

--- app/crowd-funding/src/App.js
+++ app/crowd-funding/src/App.js
@@ -1,25 +1,80 @@
-import React from "react";
+import React, {Children, FC, ReactNode, useMemo} from 'react';
 import {
   ChakraProvider,
   Box,
   Text,
   VStack,
   Grid,
+  Button,
+  useToast,
+  Code,
+  HStack,
+  Heading,
   theme,
-} from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
+  Input,
+  SimpleGrid,
+} from '@chakra-ui/react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { FormControl, FormLabel } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+import * as web3 from "@solana/web3.js";
+import {
+  ConnectionProvider,
+  WalletProvider,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletWalletAdapter,
+  SolletExtensionWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+function WalletNotConnected() {
+  return (
+    <VStack height="70vh" justify="space-around">
+      <VStack>
+        <Text fontSize="2xl">
+          {' '}
+          Looks like your wallet is not connnected. Connect a wallet to get
+          started!
+        </Text>
+        <WalletMultiButton />
+      </VStack>
+    </VStack>
+  );
+}
 
 function App() {
+
+  const network = "devnet";
+  const endpoint = web3.clusterApiUrl(network);
+  const wallets = useMemo(
+    () => [
+      PhantomWalletAdapter(),
+      SolflareWalletAdapter(),
+      SolletWalletAdapter({ network }),
+      SolletExtensionWalletAdapter({ network }),
+    ],
+    [network]
+  );
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Text>Hello world!</Text>
-          </VStack>
-        </Grid>
-      </Box>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </ChakraProvider>
   );
 }
